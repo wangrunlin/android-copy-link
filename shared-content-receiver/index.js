1c5b8c9f43f537730132885f3e3c296f9f9ff47b
Leo Wang <me@alin.run>
2024-11-12 16:17:12 +0800

    feat: get link title by fetch

--- shared-content-receiver/index.js
+++ shared-content-receiver/index.js
@@ -8,10 +8,40 @@ async function writeClipboard(text) {
   }, 3000);
 }
 
+// 发起一个网络请求获取 website title meta
+const fetchWebsiteTitle = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("网络请求失败");
+    }
+
+    const text = await response.text();
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html");
+    const title = doc.querySelector("title")?.innerText || "未找到标题";
+
+    return title;
+  } catch (error) {
+    console.error("获取标题时出错:", error);
+    return "";
+  }
+};
+
 const params = new URLSearchParams(window.location.search);
 
-const sharedName = params.get("name");
 const sharedLink = params.get("link") || params.get("text");
+let sharedName = params.get("name");
+
+if (sharedLink && !sharedName) {
+  document.getElementById("copyTitleAndLink").disabled = true;
+  fetchWebsiteTitle(sharedLink).then((title) => {
+    sharedName = title;
+    document.getElementById("title").textContent = sharedName || "未获取到标题";
+
+    document.getElementById("copyTitleAndLink").disabled = !title;
+  });
+}
 
 // 显示分享的标题
 document.getElementById("title").textContent = sharedName || "未获取到标题";
